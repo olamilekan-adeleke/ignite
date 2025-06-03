ca3c93d23981f46092193be004c37d11e5940e0a
olamilekan-adekele <olamilekanadeleke123@gmail.com>
2025-06-03 18:47:32 +0100

    chore: added color clas

---
+++ include/ui_components/color.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <include/core/SkColor.h>
+
+#include <cstdint>
+
+// Custon Color class to wrap SKColor (I Love doing things my way, hopefully i do not regret this)
+struct Color {
+  Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255);
+
+  explicit Color(uint32_t hex);
+
+  // Implicit conversion to SkColor
+  operator SkColor() const;
+
+  // Accessors
+  uint8_t r() const;
+  uint8_t g() const;
+  uint8_t b() const;
+  uint8_t a() const;
+
+  // Return a copy with new alpha
+  Color withAlpha(uint8_t newAlpha) const;
+
+  // Named constants
+  static Color Black();
+  static Color White();
+  static Color Red();
+  static Color Green();
+  static Color Blue();
+  static Color Yellow();
+  static Color Cyan();
+  static Color Magenta();
+  static Color Gray();
+  static Color LightGray();
+  static Color DarkGray();
+
+ private:
+  SkColor value;
+};
---
+++ src/ui_components/color.cpp
@@ -0,0 +1,35 @@
+#include "ui_components/color.hpp"
+
+#include <include/core/SkColor.h>
+
+Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) : value(SkColorSetARGB(a, r, g, b)) {}
+
+Color::Color(uint32_t hex) {
+  uint8_t a = (hex >> 24) & 0xFF;
+  uint8_t r = (hex >> 16) & 0xFF;
+  uint8_t g = (hex >> 8) & 0xFF;
+  uint8_t b = hex & 0xFF;
+  value = SkColorSetARGB(a, r, g, b);
+}
+
+Color::operator SkColor() const { return value; }
+
+uint8_t Color::r() const { return SkColorGetR(value); }
+uint8_t Color::g() const { return SkColorGetG(value); }
+uint8_t Color::b() const { return SkColorGetB(value); }
+uint8_t Color::a() const { return SkColorGetA(value); }
+
+Color Color::withAlpha(uint8_t newAlpha) const { return Color(r(), g(), b(), newAlpha); }
+
+// Named constants for easy color, will expand this list later on ot as i need them
+Color Color::Black() { return Color(SK_ColorBLACK); }
+Color Color::White() { return Color(SK_ColorWHITE); }
+Color Color::Red() { return Color(SK_ColorRED); }
+Color Color::Green() { return Color(SK_ColorGREEN); }
+Color Color::Blue() { return Color(SK_ColorBLUE); }
+Color Color::Yellow() { return Color(SK_ColorYELLOW); }
+Color Color::Cyan() { return Color(SK_ColorCYAN); }
+Color Color::Magenta() { return Color(SK_ColorMAGENTA); }
+Color Color::Gray() { return Color(SK_ColorGRAY); }
+Color Color::LightGray() { return Color(SK_ColorLTGRAY); }
+Color Color::DarkGray() { return Color(SK_ColorDKGRAY); }
