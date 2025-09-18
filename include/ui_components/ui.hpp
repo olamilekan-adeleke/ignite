1a1a61af65ba70c582606ce9bf35c4685eff52e3
olamilekan-adekele <olamilekanadeleke123@gmail.com>
2025-09-18 21:36:29 +0100

    refactor(layout): Remove redundant Column and Row Consolidates basic axis layout into . This simplifies the UI component architecture by eliminating specialized classes, promoting a unified approach to content arrangement, and improving API clarity.

--- include/ui_components/ui.hpp
+++ include/ui_components/ui.hpp
@@ -3,12 +3,10 @@
 #include <memory>
 #include <string>
 
-#include "column_component.hpp"
 #include "layout/h_flex_box.hpp"
 #include "layout/v_flex_box.hpp"
 #include "ui_component.hpp"
 #include "ui_sized.hpp"
-#include "row_component.hpp"
 #include "text_component.hpp"
 #include "ui_text_field.hpp"
 #include "ui_view.hpp"
@@ -22,9 +20,15 @@ inline std::shared_ptr<TextRenderer> Text(const std::string &str, const TextStyl
   return std::make_shared<TextRenderer>(str, style);
 }
 
-inline std::shared_ptr<Column> ColumnView(const ColumnParams &props = {}) { return std::make_shared<Column>(props); }
+inline std::shared_ptr<FlexBox> UIFlexBox(const FlexParam &params = {}) { return std::make_shared<FlexBox>(params); }
 
-inline std::shared_ptr<Row> RowView(const RowParams &props = {}) { return std::make_shared<Row>(props); }
+inline std::shared_ptr<FlexBox> HFlexBox(const HFlexParam &params = {}) {
+  return std::make_shared<FlexBox>(HFlexBox::create(params));
+}
+
+inline std::shared_ptr<FlexBox> VFlexBox(const VFlexParam &params = {}) {
+  return std::make_shared<FlexBox>(VFlexBox::create(params));
+}
 
 inline std::shared_ptr<View> UIView(const ViewParams &params = {}) { return std::make_shared<View>(params); }
 
@@ -42,13 +46,4 @@ inline std::shared_ptr<UITextField> UITextFieldView(const UITextFieldParams &par
   return std::make_shared<UITextField>(params);
 }
 
-inline std::shared_ptr<FlexBox> UIFlexBox(const FlexParam &params = {}) { return std::make_shared<FlexBox>(params); }
-
-inline std::shared_ptr<FlexBox> HFlexBox(const HFlexParam &params = {}) {
-  return std::make_shared<FlexBox>(HFlexBox::create(params));
-}
-
-inline std::shared_ptr<FlexBox> VFlexBox(const VFlexParam &params = {}) {
-  return std::make_shared<FlexBox>(VFlexBox::create(params));
-}
 }  // namespace UI
