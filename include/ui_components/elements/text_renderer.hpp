aa2ba011154b5050bcccc68235027a671217bb66
olamilekan-adekele <olamilekanadeleke123@gmail.com>
2025-10-31 17:18:23 +0100

    refactor(text_renderer): remove TextRenderer class and migrate to RenderText
    The TextRenderer class was removed as its functionality was redundant with the newer RenderText class. This simplifies the codebase by removing duplicate text rendering logic.

--- include/ui_components/elements/text_renderer.hpp
+++ include/ui_components/elements/text_renderer.hpp
@@ -4,49 +4,10 @@
 
 #include <string>
 
-#include "basic/ui_component.hpp"
 #include "component/component.hpp"
 #include "elements/paragraph_builder.hpp"
 #include "foundation/foundation.hpp"
 #include "foundation/utils/key.hpp"
-
-class TextRenderer : public UIComponent {
- public:
-  TextRenderer(const std::string &text, const TextStyle &style);
-
-  TextRenderer(const TextRenderer &) = delete;
-  TextRenderer &operator=(const TextRenderer &) = delete;
-
-  UISize getIntrinsicSize(UIConstraints constraints) noexcept override;
-  void layout(UIConstraints size) override;
-  void draw(SkCanvas *canvas) override;
-
- protected:
-  void debugFillProperties(std::ostringstream &os, int indent) const override;
-
- private:
-  std::string text_;
-  TextStyle style_;
-  ParagraphBuilder paragraphBuilder_;
-
-  uint64_t layoutHashCode() const noexcept {
-    const auto textKey = fnv1a(text_);
-    const auto key = fmt::format("{}-{}-{}-{}", textKey, style_.fontSize, style_.weight.toString(), style_.maxLines);
-    return fnv1a(key);
-  }
-
-  uint64_t drawHashCode() const noexcept {
-    const auto textKey = fnv1a(text_);
-    auto const key = fmt::format("{}-{}-{}-{}-{}-{}",
-                                 textKey,
-                                 style_.fontSize,
-                                 style_.weight.toString(),
-                                 style_.maxLines,
-                                 style_.textAlignment.toString(),
-                                 style_.color.toString());
-    return fnv1a(key);
-  }
-};
 
 class RenderText : public RenderObject {
  public:
--- src/ui_components/elements/text_renderer.cpp
+++ src/ui_components/elements/text_renderer.cpp
@@ -4,72 +4,8 @@
 #include <include/core/SkCanvas.h>
 #include <include/core/SkPoint.h>
 
-#include <cstdint>
-#include <sstream>
-#include <string>
-
-#include "elements/paragraph_builder.hpp"
 #include "foundation/foundation.hpp"
-#include "surface_cache_helper.hpp"
 #include "ui_element/state_base_element.hpp"
-
-TextRenderer::TextRenderer(const std::string &text, const TextStyle &style)
-    : text_(std::move(text)), style_(style), paragraphBuilder_(ParagraphBuilder(text, style)) {}
-
-UISize TextRenderer::getIntrinsicSize(UIConstraints constraints) noexcept {
-  auto size = paragraphBuilder_.getIntrinsicSize(constraints);
-  return size;
-}
-
-void TextRenderer::layout(UIConstraints size) {
-  auto childSize = paragraphBuilder_.getIntrinsicSize(size);
-  paragraphBuilder_.layout(childSize.width);
-  setSize(childSize);
-}
-
-void TextRenderer::draw(SkCanvas *canvas) {
-  const uint64_t drawHash{drawHashCode()};
-  SkRect bounds = SkRect::MakeXYWH(bounds_.x, bounds_.y, bounds_.width, bounds_.height);
-
-  SurfaceCacheHelper::drawCachedWithClip(canvas, drawHash, bounds, [&](SkCanvas *surfaceCanvas) {
-    paragraphBuilder_.draw(surfaceCanvas, SkPoint::Make(bounds_.x, bounds_.y));
-  });
-
-  UIComponent::draw(canvas);
-}
-
-inline std::string escapeString(const std::string &str) {
-  std::string result;
-  for (char c : str) {
-    switch (c) {
-      case '\\':
-        result += "\\\\";
-        break;
-      case '\n':
-        result += "\\n";
-        break;
-      case '\r':
-        result += "\\r";
-        break;
-      case '\t':
-        result += "\\t";
-        break;
-      case '"':
-        result += "\\\"";
-        break;
-      default:
-        result += c;
-    }
-  }
-  return result;
-}
-
-void TextRenderer::debugFillProperties(std::ostringstream &os, int indent) const {
-  UIComponent::debugFillProperties(os, indent);
-  std::string pad(indent, ' ');
-  paragraphBuilder_.debugFillProperties(os, indent);
-  os << pad << "text: " << fmt::format("\"{}\"", escapeString(text_)) << "\n";
-}
 
 // Text
 UIElementPtr Text::createElement() { return std::make_shared<LeafUIElement>(shared_from_this()); }
